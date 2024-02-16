2556cbeac10aef8dc8dd01282135c271a6aa483c
Rinzin Dolma <rinzindolma@Rinzins-MacBook.lan>
2024-02-16 16:19:02 -0500

    Added new classes

---
+++ src/Crib.java
@@ -0,0 +1,21 @@
+public class Crib {
+   //States
+    /*
+    - material
+    - color
+    - price
+    - brand
+    - type (mini, full-size, convertible, portable)
+    - maximum capacity
+    - weight
+    - finish
+    - suggested age
+    - has assembly instruction
+    - has warranty
+    - industry or government certification
+    - has conversion parts
+    - dimension
+    -
+    -
+*/
+}
---
+++ src/Stroller.java
@@ -0,0 +1,29 @@
+public class Stroller {
+    //States
+    /*
+    - brand
+    - price
+    - color
+    - type (travel, full size, jogger)
+    - has assembly details
+    - item number
+    - has warranty
+    - has industry certification
+    - weight limit
+    - weight
+    - has storage
+    - seat adjustments (near flat, seat recline, adjustable)
+    - has accessories (net, rain cover, snack tray, bassinet)
+    - material
+    - fabric
+    - fold
+    - suggested age
+    - can accommodate car seat
+    - product configuration (single, double)
+    - tire dimension
+    - tire wheel diameter
+    - dimensions (collapsed)
+    - dimensions (overall)
+    - has storage bag
+     */
+}
